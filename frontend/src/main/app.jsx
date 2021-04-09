b55b6790c09163592006249da19cea2468e5cc09
Daniel Nunes <danielzcomputacao@gmail.com>
2021-04-09 12:42:20 -0300

    Todo, About

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -2,9 +2,12 @@ import "modules/bootstrap/dist/css/bootstrap.min.css";
 import "modules/font-awesome/css/font-awesome.min.css";
 
 import React from "react";
+import Todo from "../todo/todo";
+import About from "../about/about";
 
 export default (props) => (
   <div className="container">
-    <h1>Teste</h1>
+    <Todo />
+    <About />
   </div>
 );
