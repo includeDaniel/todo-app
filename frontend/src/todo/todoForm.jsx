ca57c58b96010c2571ed9ea544f76794ad4d6541
Daniel Nunes <danielzcomputacao@gmail.com>
2021-04-09 16:08:37 -0300

    Form and list

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 export default (props) => (
-  <div>
-    <h1>Form</h1>
+  <div role="form" className="todoForm">
+    <div className="col-xs-12 col-sm-9 col-md-10">
+      <input
+        id="description"
+        className="form-control"
+        placeholder="Adicione uma tarefa"
+      />
+    </div>
+
+    <div className="col-xs-12 col-sm-3 col-md-2">
+      <button className="btn btn-primary">
+        <i className="fa fa-plus"></i>
+      </button>
+    </div>
   </div>
 );
--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default (props) => (
-  <div>
+  <div className="container">
     <h1>List</h1>
   </div>
 );
