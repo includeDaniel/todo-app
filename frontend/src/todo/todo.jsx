b9e8a17317abd49752ed06591eed2a2ac574fd2f
Daniel Nunes <danielzcomputacao@gmail.com>
2021-04-09 17:54:30 -0300

    fixing cors

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
+import axios from "axios";
+
 import PageHeader from "../template/pageHeader";
 import TodoForm from "./todoForm";
 import TodoList from "./todoList";
+
+const URL = "http://localhost:3003/api/todos";
 
 export default class Todo extends Component {
   constructor(props) {
@@ -10,13 +14,22 @@ export default class Todo extends Component {
 
     this.handleChange = this.handleChange.bind(this);
     this.handleAdd = this.handleAdd.bind(this);
+
+    //  this.refresh();
+  }
+
+  refresh() {
+    axios.get(`${URL}?sort=-createdAt`).then((resp) => console.log(resp.data));
   }
 
   handleChange(e) {
     this.setState({ ...this.state, description: e.target.value });
   }
-
-  handleAdd() {}
+ 
+  handleAdd() {
+    const description = this.state.description;
+    axios.post(URL, { description }).then((resp) => console.log("Funcionou"));
+  }
 
   render() {
     return (
@@ -24,6 +37,7 @@ export default class Todo extends Component {
         <PageHeader name="Tarefas" small="Cadastro"></PageHeader>
         <TodoForm
           description={this.state.description}
+          handleChange={this.handleChange}
           handleAdd={this.handleAdd}
         />
         <TodoList />
