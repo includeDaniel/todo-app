277d02bb61f4540c0055ffb1ae9dd9af96ef8fbe
Daniel Nunes <danielzcomputacao@gmail.com>
2021-04-09 13:29:27 -0300

    Form and list

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import PageHeader from "../template/pageHeader";
+import TodoForm from "./todoForm";
+import TodoList from "./todoList";
 
 export default class Todo extends Component {
   render() {
     return (
       <div>
         <PageHeader name="Tarefas" small="Cadastro"></PageHeader>
+        <TodoForm />
+        <TodoList />
       </div>
     );
   }
---
+++ frontend/src/todo/todoForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default (props) => (
+  <div>
+    <h1>Form</h1>
+  </div>
+);
