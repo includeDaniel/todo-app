c734d406417ae1d0f42d3ba93800413ea1ac0896
Daniel Nunes <danielzcomputacao@gmail.com>
2021-04-09 17:11:54 -0300

    controled component

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -4,11 +4,28 @@ import TodoForm from "./todoForm";
 import TodoList from "./todoList";
 
 export default class Todo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { description: "", list: [] };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleAdd = this.handleAdd.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ ...this.state, description: e.target.value });
+  }
+
+  handleAdd() {}
+
   render() {
     return (
       <div>
         <PageHeader name="Tarefas" small="Cadastro"></PageHeader>
-        <TodoForm />
+        <TodoForm
+          description={this.state.description}
+          handleAdd={this.handleAdd}
+        />
         <TodoList />
       </div>
     );
--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -9,11 +9,17 @@ export default (props) => (
         id="description"
         className="form-control"
         placeholder="Adicione uma tarefa"
+        onChange={props.handleChange}
+        value={props.description}
       />
     </Grid>
 
     <Grid cols="12 3 2">
-      <IconButton style="primary" icon="plus"></IconButton>
+      <IconButton
+        style="primary"
+        icon="plus"
+        onClick={props.handleAdd}
+      ></IconButton>
     </Grid>
   </div>
 );
