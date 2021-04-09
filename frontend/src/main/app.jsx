71a92bd8716c24ab2505c99bb7ab201e879b05f1
Daniel Nunes <danielzcomputacao@gmail.com>
2021-04-09 13:11:30 -0300

    Routes

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -2,14 +2,12 @@ import "modules/bootstrap/dist/css/bootstrap.min.css";
 import "modules/font-awesome/css/font-awesome.min.css";
 
 import React from "react";
-import Todo from "../todo/todo";
-import About from "../about/about";
 import Menu from "../template/menu";
+import Routes from "./routes";
 
 export default (props) => (
   <div className="container">
     <Menu />
-    <Todo />
-    <About />
+    <Routes />
   </div>
 );
