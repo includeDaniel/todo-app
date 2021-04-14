ee05c56773495ef4b4a142d52bb38a8b5ea88266
Daniel Nunes <danielzcomputacao@gmail.com>
2021-04-14 12:00:07 -0300

    creating table

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -14,12 +14,19 @@ export default class Todo extends Component {
 
     this.handleChange = this.handleChange.bind(this);
     this.handleAdd = this.handleAdd.bind(this);
+    this.handleRemove = this.handleRemove.bind(this);
+
+    this.refresh();
 
     //  this.refresh();
   }
 
   refresh() {
-    axios.get(`${URL}?sort=-createdAt`).then((resp) => console.log(resp.data));
+    axios
+      .get(`${URL}?sort=-createdAt`)
+      .then((resp) =>
+        this.setState({ ...this.state, description: "", list: resp.data })
+      );
   }
 
   handleChange(e) {
@@ -28,7 +35,11 @@ export default class Todo extends Component {
 
   handleAdd() {
     const description = this.state.description;
-    axios.post(URL, { description }).then((resp) => console.log("Funcionou !"));
+    axios.post(URL, { description }).then((resp) => this.refresh());
+  }
+
+  handleRemove(todo) {
+    axios.delete(`${URL}/${todo._id}`).then((resp) => this.refresh());
   }
 
   render() {
@@ -40,7 +51,7 @@ export default class Todo extends Component {
           handleChange={this.handleChange}
           handleAdd={this.handleAdd}
         />
-        <TodoList />
+        <TodoList list={this.state.list} handleRemove={this.handleRemove} />
       </div>
     );
   }
--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import IconButton from "../template/iconButton";
 
-export default (props) => (
-  <div>
-    <h1>List</h1>
-  </div>
-);
+export default (props) => {
+  const renderRows = () => {
+    const list = props.list || [];
+    return list.map((todo) => (
+      <tr key={todo._id}>
+        <td>{todo.description}</td>
+        <td>
+          <IconButton
+            style="danger"
+            icon="trash-o"
+            onClick={() => props.handleRemove(todo)}
+          />
+        </td>
+      </tr>
+    ));
+  };
+
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>descrição</th>
+          <th>Ações</th>
+        </tr>
+      </thead>
+      <tbody>{renderRows()}</tbody>
+    </table>
+  );
+};
