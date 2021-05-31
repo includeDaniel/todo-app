52cc46ef81819d12995e459a74e5a676dc09aa15
Daniel Nunes <danielzcomputacao@gmail.com>
2021-05-31 16:36:37 -0300

    reducers.js

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,1 +1,22 @@
 import { combineReducers } from 'redux'
+
+const rootReducer = combineReducers({
+    todo: () => ({
+        description: 'Ler livro',
+        list: [{
+            _id: 1,
+            description: 'Paga fatura do cartão',
+            done: true
+        }, {
+            _id: 2,
+            description: 'Reunião com a equipe as 10:00',
+            done: false
+        }, {
+            _id: 3,
+            description: 'Consulta médicas na aterça depois do almoço',
+            done: false
+        }]
+    })
+})
+
+export default rootReducer
