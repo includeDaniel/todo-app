49f3000c1934b48c62a2fc508fd4801fbeb894ed
Daniel Nunes <danielzcomputacao@gmail.com>
2021-05-31 17:20:08 -0300

    devTools redux

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -6,7 +6,10 @@ import { Provider } from "react-redux";
 import App from "./main/app";
 import reducers from "./main/reducers";
 
-const store = createStore(reducers);
+const devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(reducers, devTools);
 ReactDOM.render(
   <Provider store={store}>
     <App />
