3175739e58ee8621c47f3416d7d12dd5f59fb525
Daniel Nunes <danielzcomputacao@gmail.com>
2021-06-04 15:10:27 -0300

    add done

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -4,6 +4,7 @@ import { applyMiddleware, createStore } from "redux";
 import { Provider } from "react-redux";
 import promise from "redux-promise";
 import multi from "redux-multi";
+import thunk from "redux-thunk";
 
 import App from "./main/app";
 import reducers from "./main/reducers";
@@ -11,7 +12,10 @@ import reducers from "./main/reducers";
 const devTools =
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
-const store = applyMiddleware(multi, promise)(createStore)(reducers, devTools);
+const store = applyMiddleware(thunk, multi, promise)(createStore)(
+  reducers,
+  devTools
+);
 
 ReactDOM.render(
   <Provider store={store}>
--- frontend/src/todo/todoActions.js
+++ frontend/src/todo/todoActions.js
@@ -16,9 +16,9 @@ export const search = () => {
 }
 
 export const add = (description) => {
-    const request = axios.post(URL, { description })
-    return [{
-        type: 'TODO_ADDED',
-        payload: request
-    }, search()]
+    return dispatch => {
+        axios.post(URL, { description })
+            .then(resp => dispatch({ type: 'TODO_ADDED', payload: resp.data }))
+            .then(resp => dispatch(search()))
+    }
 }
